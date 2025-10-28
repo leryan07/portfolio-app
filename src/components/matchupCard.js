abb0526b45bc0e6836e6d6b3f9543776db46d2de
Ryan <leryan07@gmail.com>
2025-10-28 11:27:21 -0500

    Add arrow to show winner of matchup

--- src/components/matchupCard.js
+++ src/components/matchupCard.js
@@ -2,6 +2,7 @@ import { Box, Typography } from '@mui/material';
 import styled from '@emotion/styled';
 import { convertToLocalTime } from '../utils/dateUtil';
 import { useTranslation } from 'react-i18next';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
 
 const BoxStyled = styled(Box)(({ theme }) => ({
     padding: '8px',
@@ -14,6 +15,21 @@ const BoxStyled = styled(Box)(({ theme }) => ({
 export const MatchupCard = ({ matchup }) => {
     const { t } = useTranslation();
     const matchupState = matchup.status.type.state;
+    const homeTeam = matchup.home;
+    const awayTeam = matchup.away;
+    const homeScore = Number(matchup.homeScore);
+    const awayScore = Number(matchup.awayScore);
+
+    let winner;
+    if (matchup.league === 'nfl' && (homeScore === awayScore)) {
+        winner = 'tie';
+    } else if (homeScore > awayScore) {
+        winner = homeTeam;
+    } else if (homeScore < awayScore) {
+        winner = awayTeam;
+    } else {
+        null
+    }
 
     return (
         <BoxStyled width="fit-content">
@@ -23,33 +39,50 @@ export const MatchupCard = ({ matchup }) => {
                         `Week ${matchup.weekNumber}` : ''
                     }
                 </Typography>
-                <Typography variant="body1">
-                    {matchupState === 'post' ? t('final') :
-                        convertToLocalTime(matchup.date)
-                    }
-                </Typography>
+                <Box display="flex" alignItems="center" justifyContent="flex-end">
+                    <Typography variant="body1" sx={{ textAlign: 'right', flexGrow: 1 }}>
+                        {matchupState === 'post' ? t('final') :
+                            convertToLocalTime(matchup.date)
+                        }
+                    </Typography>
+                    {matchupState === 'post' && (
+                        <Box sx={{ width: 24, height: 24, display: 'flex' }} />
+                    )}
+                </Box>
             </Box>
             <Box display="flex" justifyContent="space-between" sx={{ mt: 1 }}>
                 <Typography variant="body1" sx={{ minWidth: 200 }}>
                     {matchup.away}
                 </Typography>
-                <Typography variant="body1">
-                    {matchupState === 'pre' ?
-                        matchup.awayTeamOverallRecord :
-                        matchup.awayScore
+                <Box display="flex" alignItems="center" justifyContent="flex-end">
+                    <Typography variant="body1" sx={{ textAlign: 'right', flexGrow: 1 }}>
+                        {matchupState === 'pre' ? matchup.awayTeamOverallRecord : matchup.awayScore}
+                    </Typography>
+                    {
+                        matchupState === 'post' && (
+                            <Box sx={{ width: 24, height: 24, display: 'flex' }}>
+                                {winner === awayTeam && <ArrowLeftIcon />}
+                            </Box>
+                        )
                     }
-                </Typography>
+                </Box>
             </Box>
             <Box display="flex" justifyContent="space-between">
                 <Typography variant="body1" sx={{ minWidth: 200 }}>
                     {matchup.home}
                 </Typography>
-                <Typography variant="body1">
-                    {matchupState === 'pre' ?
-                        matchup.homeTeamOverallRecord :
-                        matchup.homeScore
+                <Box display="flex" alignItems="center" justifyContent="flex-end">
+                    <Typography variant="body1" sx={{ textAlign: 'right', flexGrow: 1 }}>
+                        {matchupState === 'pre' ? matchup.homeTeamOverallRecord : matchup.homeScore}
+                    </Typography>
+                    {
+                        matchupState === 'post' && (
+                            <Box sx={{ width: 24, height: 24, display: 'flex' }}>
+                                {winner === homeTeam && <ArrowLeftIcon />}
+                            </Box>
+                        )
                     }
-                </Typography>
+                </Box>
             </Box>
         </BoxStyled>
     )
